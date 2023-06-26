c0781dd3a34655ec9ec4ac980c00372ef033ef71
xzWhat <115475302+coffeedipso@users.noreply.github.com>
2023-06-26 17:32:30 +0300

    Add files via upload

--- JournalProject/JS/script.js
+++ JournalProject/JS/script.js
@@ -1,7 +1,9 @@
-let Student = ["ggreger", "rkhwhkr", "h;length;lhl"];
+let Student = ["ggreger", "rkhwhkr", "h;length;lhl", 'helloworld'];
 gradebook = document.querySelector(".gradebook");
 students = document.querySelector(".students");
+aver = document.querySelector(".aver");
 marks = document.querySelector(".marks");
+result = document.querySelector(".result");
 let counterMarksBlocks = 100;
 Student.forEach((st) => {
 	let line = document.createElement("div");
@@ -17,7 +19,62 @@ Student.forEach((st) => {
 	linemark.classList.add("linemark");
 	for (let i = 0; i < counterMarksBlocks; i++) {
 		let mark = document.createElement("input");
+		mark.setAttribute("type", "number");
 		linemark.appendChild(mark);
 	}
 	marks.appendChild(linemark);
+	let sr = document.createElement('div');
+	sr.classList.add("sr");
+	aver.appendChild(sr);
+
+	let res = document.createElement('input');
+	res.setAttribute("type", "number");
+	res.classList.add("res");
+	result.appendChild(res);
+});
+
+
+const timeForm = document.querySelector(".marks");
+const inputs = [...timeForm.querySelectorAll("input")];
+
+let currentActiveIndex = 0;
+inputs[currentActiveIndex].focus();
+inputs.forEach((input, index) =>
+	input.addEventListener("click", () => (currentActiveIndex = index))
+);
+
+timeForm.addEventListener("keydown", (e) => {
+	let direction = 0;
+	switch (e.code) {
+		case "ArrowRight":
+			direction = 1;
+			break;
+		case "ArrowLeft":
+			direction = -1;
+			break;
+		case "ArrowUp":
+			direction = -100;
+			break;
+		case "ArrowDown":
+			direction = 100;
+			break;
+		default:
+			return;
+	}
+	currentActiveIndex =
+		(inputs.length + currentActiveIndex + direction) % inputs.length;
+	inputs[currentActiveIndex].focus();
+});
+
+
+let blocks = document.querySelectorAll("input");
+blocks.forEach((block) => {
+	block.addEventListener("keydown", (e) => {
+		if (e.code.includes("Arrow")) {
+			e.preventDefault();
+			if (block.value > 5 || block.value < 2 || isNaN(block.value)) {
+				block.value = "";
+			}
+		}
+	});
 });
